c26a5d966169d252130466a782c1fbc0ed2a32ca
dlopez079 <dlopez@mattmen.com>
2021-07-27 16:34:22 -0400

    Welcome Blade. I revised the welcome blade main color from purple to red.  I changed the background image from computer base to chimney base.  I added a new background image.  I removed the amount of links that were located above the hero.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,32 +10,32 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+    @livewireStyles
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
 </head>
 
-{{-- <body class="antialiased">
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                @auth
-                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
-                @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-
-
-  
-  
-    </div>
-</body> --}}
-
 <body>
-    @livewire('hero')
+    {{-- Make sure that the Sign in and Register button are working.  --}}
+    
+    {{-- May need to seperate the navbar from the Hero. --}}
+
+    {{-- Insert Hero Here. --}}
+    @livewire('hero');
+
+    {{-- Testimonials --}}
+
+    {{-- About the company / founder --}}
+
+    {{-- Contact Form --}}
+
+    @livewireScripts
 </body>
 
 </html>
