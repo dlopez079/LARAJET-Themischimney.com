748c0f43fec070bb77d517ce12a6cd217cef9534
David Lopez <dlopez@mattmen.com>
2021-09-10 14:28:18 -0400

    Contact Form Component. I added a form to the contact form component.  I also modified the information to match Themis's contact information.

--- resources/views/livewire/about.blade.php
+++ resources/views/livewire/about.blade.php
@@ -1,5 +1,21 @@
-<div>
-    <div>
-        This is the about component!
+<div id="about">
+    {{-- About --}}
+    <div class="py-5">
+        <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="relative py-24 px-8 bg-red-500 rounded-xl shadow-2xl overflow-hidden lg:px-16 lg:grid lg:grid-cols-2 lg:gap-x-8">
+            <div class="absolute inset-0 opacity-50 filter saturate-0 mix-blend-multiply">
+            <img src="https://images.unsplash.com/photo-1512584927346-c53de69dec17?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1056&q=80" alt="Chimney Fabrication Factory" class="w-full h-full object-cover">
+            </div>
+            <div class="relative lg:col-span-2">
+                <div class="mt-6 text-white">
+                    <h1 class="text-xl font-medium sm:text-2xl text-center">About Us</h1>
+                    <footer class="mt-6">
+                        <p class="flex flex-col font-medium">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, asperiores quam. Fuga eos voluptatibus animi eligendi soluta, obcaecati molestias iusto facilis voluptate delectus provident necessitatibus consequatur, quo, quae labore velit atque voluptas dolores consectetur quas fugit. Ipsam inventore cum ab voluptas, totam doloribus nam cumque, aut vero, labore ratione magnam nostrum necessitatibus suscipit numquam aperiam obcaecati earum ut a explicabo rem minus provident optio esse! Expedita quas sed molestiae assumenda excepturi necessitatibus error asperiores quaerat fugiat quasi velit aspernatur accusantium, eius modi laboriosam cumque quis, laborum dolores, saepe mollitia inventore ullam optio. Alias repellendus possimus laborum cumque expedita, ab et!
+                        </p>
+                    </footer>           
+                </div>            
+            </div>
+        </div>
     </div>
 </div>
