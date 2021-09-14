8231433aae6e5823767622311cf68cf66e7db959
David Lopez <dlopez@mattmen.com>
2021-09-14 15:45:05 -0400

    Remove Authentication Buttons. I removed the Signin and registered authenitication buttons for now so I can publish the new website.

--- resources/views/livewire/hero.blade.php
+++ resources/views/livewire/hero.blade.php
@@ -44,16 +44,16 @@
 
                     @auth
                         <a href="{{ url('/dashboard') }}"
-                            class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Home</a>
+                            class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900 hidden">Home</a>
                     @else
                         <a href="{{ route('login') }}"
-                            class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                            class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900 hidden">
                             Sign in
                         </a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}"
-                                class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-600 hover:bg-red-700">
+                                class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-600 hover:bg-red-700 hidden">
                                 Register
                             </a>
                         @endif
