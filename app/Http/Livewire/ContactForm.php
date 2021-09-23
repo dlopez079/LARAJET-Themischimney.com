21b160df43fa1750f66ef52eb7217a9fef2f71dc
dlopez079 <dlopez@mattmen.com>
2021-09-23 16:32:01 -0400

    Contact Migration. I created a migration and model for contacts.  I also created configured the contactForm to push data to the mysql table.

--- app/Http/Livewire/ContactForm.php
+++ app/Http/Livewire/ContactForm.php
@@ -3,9 +3,30 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Contact;
 
 class ContactForm extends Component
 {
+    // Create variable for form field data.
+    public $name;
+    public $email;
+    public $tel;
+    public $message;
+   
+    public function submit()
+    {
+        $validatedData = $this->validate([
+            'name' => 'required|min:6',
+            'email' => 'required|email',
+            'tel' => 'required|max:15',
+            'message' => 'required|max:150'
+        ]);
+   
+        Contact::create($validatedData);
+   
+        // return redirect()->to('/form');
+    }
+
     public function render()
     {
         return view('livewire.contact-form');
--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -55,20 +55,20 @@
                                     </div>
 
                                     <!-- Form -->
-                                    <form class="p-6 flex flex-col justify-center">
+                                    <form wire:submit.prevent="update" class="p-6 flex flex-col justify-center">
                                         <div class="flex flex-col">
                                             <label for="name" class="hidden">Full Name</label>
-                                            <input type="name" name="name" id="name" placeholder="Full Name" class="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-red-500 focus:outline-none">
+                                            <input wire:model="name" type="name" name="name" id="name" placeholder="Full Name" class="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-red-500 focus:outline-none">
                                         </div>
 
                                         <div class="flex flex-col mt-2">
                                             <label for="email" class="hidden">Email</label>
-                                            <input type="email" name="email" id="email" placeholder="Email" class="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-red-500 focus:outline-none">
+                                            <input wire:model="email" type="email" name="email" id="email" placeholder="Email" class="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-red-500 focus:outline-none">
                                         </div>
 
                                         <div class="flex flex-col mt-2">
                                             <label for="tel" class="hidden">Number</label>
-                                            <input type="tel" name="tel" id="tel" placeholder="Telephone Number" class="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-red-500 focus:outline-none">
+                                            <input wire:model="tel" type="tel" name="tel" id="tel" placeholder="Telephone Number" class="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-red-500 focus:outline-none">
                                         </div>
 
                                         <button type="submit" class="md:w-32 bg-red-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-red-500 transition ease-in-out duration-300">
