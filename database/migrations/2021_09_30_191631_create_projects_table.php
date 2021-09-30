bacd264bff74df01003ce7468aa0762701027af1
David Lopez <dlopez@mattmen.com>
2021-09-30 15:38:13 -0400

    Projects Table. I created a Projects table with the following columns -> primary key id, foreign key team_id, foreign key user_id, name, street, city, state, zip, referall, and description.   After running the migration command I confirmed that the table was created on table plus.  In addition to the models, factories, migration and seeder was also created.

---
+++ database/migrations/2021_09_30_191631_create_projects_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('team_id');
+            $table->foreignId('user_id');
+            $table->string('name');
+            $table->string('street');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
+            $table->string('referral');
+            $table->longText('description');
+            $table->timestamps();
+
+            $table->unique(['team_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
