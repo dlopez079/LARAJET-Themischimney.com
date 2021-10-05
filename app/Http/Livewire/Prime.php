51dd826a057fe077b57eea82f1814bbf60915079
dlopez079 <dlopez@mattmen.com>
2021-10-05 00:09:55 -0400

    Prime Livewire Component. I created a livewire component and controller named prime for my front end.

---
+++ app/Http/Livewire/Prime.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Prime extends Component
+{
+    public function render()
+    {
+        return view('livewire.prime');
+    }
+}
