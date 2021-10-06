db39ea19474ea5d3a277622e35ace975b8caf386
dlopez079 <dlopez@mattmen.com>
2021-10-06 13:17:54 -0400

    Timesheet Livewire Component Contruction. I constructed the controller to manage the information that will go onto the database from the form that I will create.

--- app/Http/Livewire/TimesheetForm.php
+++ app/Http/Livewire/TimesheetForm.php
@@ -3,11 +3,63 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\timesheet;
+use Cviebrock\EloquentSluggable\Services\SlugService;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rules\Unique;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class TimesheetForm extends Component
 {
+    public $name;
+    public $slug;
+    public $project;
+    public $city;
+    public $timeIn;
+    public $timeOut;
+    public $hours;
+    public $user_id;
+    public $showModalForm = false;
+
+    public function showCreateTimesheetModal() {
+        
+        // Change the public variable showModelForm from false to true because you are going from not showing the modal to showing it. 
+        $this->showModalForm = true;
+
+    }
+
+    // Create a method for storetimesheet for the Create timesheet Modal
+    public function storeTimesheet(){
+
+        // Gather information from the Create timesheet Modal and enter it into MySql.
+        $this->validate([
+            'name' => 'required',
+            'project' => 'required',
+            'city' => 'required',
+            'timeIn' => 'required',
+            'timeOut' => 'required',
+            'hours' => 'required',
+          ]);
+
+        $timesheet =new timesheet();
+        $timesheet->user_id = auth()->user()->id;
+        $timesheet->name = $this->name;
+        $timesheet->slug = $this->slug;
+        $timesheet->project = $this->project;
+        $timesheet->city = $this->city;
+        $timesheet->timeIn = $this->timeIn;
+        $timesheet->timeOut = $this->timeOut;
+        $timesheet->hours = $this->hours;
+  
+        $timesheet->save();
+        $this->reset();
+    }
+
+
     public function render()
     {
-        return view('livewire.timesheet-form');
+        return view('livewire.timesheet-form', [
+            'timesheets' => timesheet::all()
+        ]);
     }
 }
