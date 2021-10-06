e9d539a2fc547ad202e60143bc2c4bbb29a605f5
dlopez079 <dlopez@mattmen.com>
2021-10-06 13:01:11 -0400

    Timesheet Backend. I created the timesheet migration, model, seeder and factory.  I revised the migration with new columns that fit what the company required.  I contructed the model the same way the other models were contructed. I created the livewire component and controller.  I did not provide any modifications for these blades yet.

---
+++ database/migrations/2021_10_06_164252_create_timesheets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimesheetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timesheets', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->string('project_name');
+            $table->string('city');
+            $table->string('timeIn');
+            $table->string('timeOut');
+            $table->string('hours');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timesheets');
+    }
+}
