b4cd6a7667988cca6c671078e16cfe3a5427a430
dlopez079 <dlopez@mattmen.com>
2021-10-06 14:29:57 -0400

    Timesheet-Form Modification. I modified the form so it takes it the appropriate data to place on the MySql database.

--- resources/views/livewire/timesheet-form.blade.php
+++ resources/views/livewire/timesheet-form.blade.php
@@ -10,29 +10,30 @@
     <!-- timesheet Card: We will insert for each statement here. ----------------------------------------------------------------------->
     <div>
 
-    @foreach($timesheets as $timesheet)
-        <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-            <li class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
-                <div class="w-full flex items-center justify-between p-6 space-x-6">
-                    <div class="flex-1 truncate">
-                        <div class="flex items-center space-x-3">
-                            <h3 class="text-gray-900 text-sm font-medium truncate">{{ $timesheet->name }}</h3>
-                            {{-- <span class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">{{ $timesheet->status }}</span> --}}
+        @foreach($timesheets as $timesheet)
+            <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                <li class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
+                    <div class="w-full flex items-center justify-between p-6 space-x-6">
+                        <div class="flex-1 truncate">
+                            <div class="flex items-center space-x-3">
+                                <h3 class="text-gray-900 text-sm font-medium truncate">{{ $timesheet->name }}</h3>
+                                {{-- <span class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">{{ $timesheet->status }}</span> --}}
+                            </div>
+
+                            <p class="mt-1 text-gray-500 text-sm truncate">Street: {{ $timesheet->date }}</p>
+                            <p class="mt-1 text-gray-500 text-sm truncate">City: {{ $timesheet->city }} </p>
+                            <p class="mt-1 text-gray-500 text-sm truncate">State: {{ $timesheet->timeIN }}</p>
+                            <p class="mt-1 text-gray-500 text-sm truncate">Zip: {{ $timesheet->timeOut }}</p>
+                            <p class="mt-1 text-gray-500 text-sm truncate">Contact: {{ $timesheet->hours }}</p>
+
                         </div>
 
-                        <p class="mt-1 text-gray-500 text-sm truncate">Street: {{ $timesheet->date }}</p>
-                        <p class="mt-1 text-gray-500 text-sm truncate">City: {{ $timesheet->city }} </p>
-                        <p class="mt-1 text-gray-500 text-sm truncate">State: {{ $timesheet->timeIN }}</p>
-                        <p class="mt-1 text-gray-500 text-sm truncate">Zip: {{ $timesheet->timeOut }}</p>
-                        <p class="mt-1 text-gray-500 text-sm truncate">Contact: {{ $timesheet->hours }}</p>
-
                     </div>
+                </li>
 
-                </div>
-            </li>
+            </ul>
+        @endforeach
 
-        </ul>
-    @endforeach
     </div>
 
 
@@ -112,7 +113,7 @@
 
     <!-- Modal Footer -->
         <x-slot name="footer">
-            <x-jet-button wire:click="storetimesheet">Save timesheet</x-jet-button>
+            <x-jet-button wire:click="storeTimesheet">Save timesheet</x-jet-button>
         </x-slot>
 
     </x-jet-dialog-modal>
