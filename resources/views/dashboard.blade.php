dce4c46e8eae113f6d3a38834f165f53b02b981c
David Lopez <dlopez@mattmen.com>
2021-10-18 10:40:14 -0400

    Dashboard - Enter Time. I added the timesheets route to the 'Enter Time' section so that users can navigate to enter their time via the dashboard main section.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -43,7 +43,7 @@
     </div>
     <div class="mt-8">
       <h3 class="text-lg font-medium">
-        <a href="#" class="focus:outline-none">
+        <a href="{{ route('timesheets') }}" class="focus:outline-none">
           <!-- Extend touch target to entire panel -->
           <span class="absolute inset-0" aria-hidden="true"></span>
           Enter Time
