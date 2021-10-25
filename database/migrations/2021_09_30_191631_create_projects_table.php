dc851d43927a1b006a5b0b12c618c7e5c76a87e1
David Lopez <dlopez@mattmen.com>
2021-10-25 12:26:23 -0400

    Project Table Mofication I re-organized the project table to prepare it for relationships.

--- database/migrations/2021_09_30_191631_create_projects_table.php
+++ database/migrations/2021_09_30_191631_create_projects_table.php
@@ -18,12 +18,12 @@ class CreateProjectsTable extends Migration
             $table->foreignId('user_id'); 
             $table->string('project_name');
             $table->string('slug')->unique();
-            $table->string('general_contractor');
-            $table->foreignId('client_id')->nullable();
-            $table->string('street')->nullable();
-            $table->string('city')->nullable();
+            $table->foreignId('general_contractor_id');
+            $table->foreignId('client_id');
+            $table->string('street');
+            $table->string('city');
             $table->string('state')->default('NY');
-            $table->string('zip')->nullable();
+            $table->string('zip');
             $table->longText('description')->nullable();
             $table->string('status')->default('Active');
             $table->timestamps();
