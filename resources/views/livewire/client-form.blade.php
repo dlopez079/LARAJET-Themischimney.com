8ab2ffd26c9dc9745ca3d6c40c08fa905cf3b07f
David Lopez <dlopez@mattmen.com>
2021-10-25 14:44:42 -0400

    Recreated client lists

--- resources/views/livewire/client-form.blade.php
+++ resources/views/livewire/client-form.blade.php
@@ -1,69 +1,174 @@
 <div>
-    <!-- Project Buttons -->
+    <!-- Client Buttons -->
     <div class="text-center p-3 mx-auto">
-   
+
         <x-jet-button wire:click="showCreateClientModal">
             New Client
         </x-jet-button>
     </div>
 
-    <!-- Client Card: We will insert for each statement here. ----------------------------------------------------------------------->
-    <div>
+    <!-- Client List: We will insert for each statement here. ----------------------------------------------------------------------->
+    <div class="bg-white shadow overflow-hidden sm:rounded-md">
+        <ul role="list" class="divide-y divide-gray-200">
 
-    @foreach($clients as $client)
-        <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-            <li class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
-                <div class="w-full flex items-center justify-between p-6 space-x-6">
-                    <div class="flex-1 truncate">
-                        <div class="flex items-center space-x-3">
-                            <h3 class="text-gray-900 text-sm font-medium truncate">{{ $client->name }}</h3>
-                            <span class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">{{ $client->status }}</span>
+            @foreach($clients as $client)
+            <li>
+                <a href="#" class="block hover:bg-gray-50">
+                    <div class="px-4 py-4 sm:px-6">
+                        <div class="flex items-center justify-between">
+                            <p class="text-sm font-medium text-indigo-600 truncate">
+                                {{ $client->name }}
+                            </p>
+                            <div class="ml-2 flex-shrink-0 flex">
+                                <p class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                    {{ $client->status}}
+                                </p>
+                            </div>
                         </div>
-                        <p class="mt-1 text-gray-500 text-sm truncate">Street: {{ $client->street }}</p>
-                        <p class="mt-1 text-gray-500 text-sm truncate">City: {{ $client->city }} </p>
-                        <p class="mt-1 text-gray-500 text-sm truncate">State: {{ $client->state }}</p>
-                        <p class="mt-1 text-gray-500 text-sm truncate">Zip: {{ $client->zip }}</p>
-                        <p class="mt-1 text-gray-500 text-sm truncate">Contact: {{ $client->contact }}</p>
-                        <p class="mt-1 text-gray-500 text-sm truncate">Phone: {{ $client->phone }}</p>
-                        <p class="mt-1 text-gray-500 text-sm truncate">Email: {{ $client->email }}</p>
-                        <p class="mt-1 text-gray-500 text-sm truncate">Website: {{ $client->website }}</p>
-                        <p class="mt-1 text-gray-500 text-sm truncate">Description: {{ $client->description }}</p>
-
-                    </div>
-
-                    <!-- Image -->
-                    <img class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
-                </div>
-                <div>
-                    <div class="-mt-px flex divide-x divide-gray-200">
-                        <div class="w-0 flex-1 flex">
-                            <a href="mailto:{{ $client->email }}" class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
-
-                                <!-- Heroicon name: solid/mail -->
-                                <svg class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                                </svg>
-                                <span class="ml-3">Email</span>
-                            </a>
-                        </div>
-                        <div class="-ml-px w-0 flex-1 flex">
-                            <a href="tel:+1-{{ $client->phone }}" class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
-
-                                <!-- Heroicon name: solid/phone -->
+                        <div class="mt-2 sm:flex sm:justify-between">
+                            <div class="sm:flex">
+                                <p class="flex items-center text-sm text-gray-500">
+                                    <!-- Heroicon name: solid/users -->
+                                    <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+                                    </svg>
+                                    {{ $client->contact}}
+                                </p>
+                                <p class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6">
+                                    <!-- Heroicon name: solid/location-marker -->
+                                    <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                                        <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                                    </svg>
+                                    {{ $client->email }}
+                                </p>
+                            </div>
+                            <div class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                                <!-- Heroicon name: solid/calendar -->
                                 <svg class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                                 </svg>
-                                <span class="ml-3">Call</span>
-                            </a>
+                                <p>
+                                    {{ $client->phone }}
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </li>
-
+            @endforeach
         </ul>
-    @endforeach
+
+
     </div>
+
+    <div class="py-2">
+        {{ $clients->links() }}
+    </div>
+
+
+    <!-- Client card 
+    When a user selects an contact from the list, I want the full contact to display underneath. ----------------------------------->
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+        <div class="px-4 py-5 sm:px-6">
+            <h3 class="text-lg leading-6 font-medium text-gray-900">
+                Company Information
+            </h3>
+            <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                123 Address St., City, ST, Zip
+            </p>
+        </div>
+        <div class="border-t border-gray-200 px-4 py-5 sm:px-6">
+            <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                <div class="sm:col-span-1">
+                    <dt class="text-sm font-medium text-gray-500">
+                        Contact name
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900">
+                        Margot Foster
+                    </dd>
+                </div>
+                <div class="sm:col-span-1">
+                    <dt class="text-sm font-medium text-gray-500">
+                        Telephone
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900">
+                        (123) 456-7890
+                    </dd>
+                </div>
+                <div class="sm:col-span-1">
+                    <dt class="text-sm font-medium text-gray-500">
+                        Email address
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900">
+                        margotfoster@example.com
+                    </dd>
+                </div>
+                <div class="sm:col-span-1">
+                    <dt class="text-sm font-medium text-gray-500">
+                       Website
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900">
+                        www.site.com
+                    </dd>
+                </div>
+                <div class="sm:col-span-2">
+                    <dt class="text-sm font-medium text-gray-500">
+                        About / Description
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900">
+                        Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+                    </dd>
+                </div>
+                <div class="sm:col-span-2">
+                    <dt class="text-sm font-medium text-gray-500">
+                        Attachments
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900">
+                        <ul role="list" class="border border-gray-200 rounded-md divide-y divide-gray-200">
+                            <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                <div class="w-0 flex-1 flex items-center">
+                                    <!-- Heroicon name: solid/paper-clip -->
+                                    <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
+                                    </svg>
+                                    <span class="ml-2 flex-1 w-0 truncate">
+                                        resume_back_end_developer.pdf
+                                    </span>
+                                </div>
+                                <div class="ml-4 flex-shrink-0">
+                                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+                                        Download
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                <div class="w-0 flex-1 flex items-center">
+                                    <!-- Heroicon name: solid/paper-clip -->
+                                    <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
+                                    </svg>
+                                    <span class="ml-2 flex-1 w-0 truncate">
+                                        coverletter_back_end_developer.pdf
+                                    </span>
+                                </div>
+                                <div class="ml-4 flex-shrink-0">
+                                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+                                        Download
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </dd>
+                </div>
+            </dl>
+        </div>
+    </div>
+
+
+
+
 
 
     <!-- Modal Form for Client Entry ------------------------------------------------------------------------------------------------>
@@ -96,7 +201,7 @@
                         </label>
                         <input wire:model.lazy="street" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-street" name="grid-street" type="text" placeholder="Enter Street">
                     </div>
-                    
+
                     <!-- City Field -->
                     <div class="w-full px-5 mb-6">
                         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
@@ -171,7 +276,7 @@
             </form>
         </x-slot>
 
-    <!-- Modal Footer -->
+        <!-- Modal Footer -->
         <x-slot name="footer">
             <x-jet-button wire:click="storeClient">Save Client</x-jet-button>
         </x-slot>
