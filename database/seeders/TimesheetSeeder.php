4de81064b32d12cba5fe666e0b2bc7a1a13168f8
David Lopez <dlopez@mattmen.com>
2021-10-25 10:38:47 -0400

    Timeshee Seeder. I tempted to add a seeder to timesheets but ran into many problems.  I will self this for now and come back to it.  I did, however, leave notes on the file so I can test out laster.

--- database/seeders/TimesheetSeeder.php
+++ database/seeders/TimesheetSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Schema\ForeignIdColumnDefinition;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class TimesheetSeeder extends Seeder
 {
@@ -13,6 +17,13 @@ class TimesheetSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Timesheet database is going to have the following fields
+        // Date > Create a seed for random date
+        // Project > This will change once relationships are established
+        // City > Create a seed for random city string
+        // Time In > Create a seed for random time.
+        // Time Out > Create a seed for random time. 
+        // Hours > Create a seed for random time string. 
+
     }
 }
