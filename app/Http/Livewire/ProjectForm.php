144285e6f0aa2965f87234e41ad561a585b506fc
David Lopez <dlopez@mattmen.com>
2021-10-28 13:07:46 -0400

    ProjectForm Controller Modifications. I removed some validation since I'm not gonig to work on those fields right now.  I'm going to focus on the attachment field and re-enable all other fields later.

--- app/Http/Livewire/ProjectForm.php
+++ app/Http/Livewire/ProjectForm.php
@@ -7,13 +7,16 @@ use App\Models\Project;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rules\Unique;
 use Cviebrock\EloquentSluggable\Services\SlugService;
+use Livewire\WithFileUploads;
 
 class ProjectForm extends Component
 {
 
+    use WithFileUploads;
+
     public $project_name;
     public $general_contractor;
-    public $client;
+    // public $client;
     public $street;
     public $city;
     public $state;
@@ -21,9 +24,9 @@ class ProjectForm extends Component
     public $description;
     public $status;
     public $user_id;
-    public $count;
     public $showModalForm = false;
     public $slug;
+    public $attachments;
     // public $slug = SlugService::createSlug(Project::class, 'slug', 'My First Post');
 
     public function showCreateProjectModal() {
@@ -39,21 +42,21 @@ class ProjectForm extends Component
         // Gather information from the Create Project Modal and enter it into MySql.
         $this->validate([
             'project_name' => 'required',
-            'general_contractor' => 'required',
+            // 'general_contractor_id' => 'required',
             // 'client' => 'required',
             'street' => 'required',
             'city' => 'required',
-            // 'state' => 'required',
             'zip' => 'required',
             'description' => 'required',
-            'status' => 'required'
+            'status' => 'required',
+            // 'attachments' => 'file|mimes:pdf,jpg,png|max:102400 ', // 12MB Max, and must be an image or pdf.
         ]);
 
         $project =new Project();
         $project->user_id = auth()->user()->id;
         $project->project_name = $this->project_name;
         $project->slug = $this->slug;
-        $project->general_contractor = $this->general_contractor;
+        // $project->general_contractor_id = $this->general_contractor;
         // $project->client = $this->client;
         $project->street = $this->street;
         $project->city = $this->city;
@@ -61,6 +64,7 @@ class ProjectForm extends Component
         $project->zip = $this->zip;
         $project->description = $this->description;
         $project->status = $this->status;
+        // $project->attachments = $this->attachments;
         
         $project->save();
         $this->reset();
