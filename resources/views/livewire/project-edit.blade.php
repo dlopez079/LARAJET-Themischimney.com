aae48d2113bc934d4d4ac79940e781463d29833e
David Lopez <dlopez@mattmen.com>
2021-10-28 18:49:21 -0400

    Project-Edit Blade Modifications. I added the wire model tag along with an id to the project edit details so I can pull up details when I click on the company listings.

--- resources/views/livewire/project-edit.blade.php
+++ resources/views/livewire/project-edit.blade.php
@@ -1,4 +1,5 @@
-<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+<div wire:model="showProjectCard" id="projectCard" class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
     <div class="mt-6 text-gray-500 text-center pb-3">
         Select Client Above to display Details.
     </div>
@@ -85,13 +86,23 @@
                         Attachments
                     </dt>
                     <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+
+                        <!-- Beginningof Unordered List of Attachments -->
                         <ul role="list" class="border border-gray-200 rounded-md divide-y divide-gray-200">
+
+                            <!-- 
+                                Attachment 1
+                                The 'For Loop' to display list of attachment should go here.
+                             -->
                             <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
                                 <div class="w-0 flex-1 flex items-center">
+
                                     <!-- Heroicon name: solid/paper-clip -->
                                     <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                         <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
                                     </svg>
+
+                                    <!-- Name of File -->
                                     <span class="ml-2 flex-1 w-0 truncate">
                                         Sketcher_Drawings.pdf
                                     </span>
@@ -102,6 +113,11 @@
                                     </a>
                                 </div>
                             </li>
+
+                            <!-- 
+                                Attachment 2
+                                The 'End For Loop' to finish the loop should go here.  We can remove the second list item.
+                             -->
                             <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
                                 <div class="w-0 flex-1 flex items-center">
                                     <!-- Heroicon name: solid/paper-clip -->
@@ -120,11 +136,6 @@
                             </li>
                         </ul>
                     </dd>
-                </div>
-
-
-                <div align="center">
-                    <x-jet-button>Back to Project List</x-jet-button>
                 </div>
 
             </dl>
