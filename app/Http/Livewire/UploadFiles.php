ff531ec7baea5707d939031c5d4cb37706a38e73
David Lopez <dlopez@mattmen.com>
2021-11-08 15:31:43 -0500

    Uploade Component. I moved away from using the upload component but I intend to use it in the future so I kept it and updated it with all the different versions of Livewire storage functions.

--- app/Http/Livewire/UploadFiles.php
+++ app/Http/Livewire/UploadFiles.php
@@ -15,18 +15,35 @@ class UploadFiles extends Component
     public function save()
     {
         $this->validate([
-            'files.*' => 'file|mimes:pdf,jpg,png|max:102400 ', // 12MB Max, and must be an image or pdf.
+            'files.*' => 'file|max:102400 ', // 12MB Max
         ]);
  
         foreach ($this->files as $file) {
-            $file->storeAs('files', $file);
+
+            // Store the uploaded file in the "files" directory of the default filesystem disk.
+            $file->store('files');
+            
+            // Store in the "files" directory in a configured "s3" bucket.
+            $file->store('files', 's3');
+            
+            // Store in the "files" directory with the filename "avatar.png".
+            $file->storeAs('files', 'avatar');
+            
+            // Store in the "files" directory in a configured "s3" bucket with the filename "avatar.png".
+            $file->storeAs('files', 'avatar', 's3');
+            
+            // Store in the "files" directory, with "public" visibility in a configured "s3" bucket.
+            $file->storePublicly('files', 's3');
+            
+            // Store in the "files" directory, with the name "avatar.png", with "public" visibility in a configured "s3" bucket.
+            $file->storePubliclyAs('files', 'avatar', 's3');
         }
         
     }
 
 
-    // public function render()
-    // {
-    //     return view('livewire.upload-files');
-    // }
+    public function render()
+    {
+        return view('livewire.upload-files');
+    }
 }
