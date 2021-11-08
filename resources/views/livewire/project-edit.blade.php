5766a9b3d8be0406687ef4fa5788e8bb1f79b0da
David Lopez <dlopez@mattmen.com>
2021-11-08 15:28:13 -0500

    Attachment Display. I displayed the list of attachments on the project edit page.  When a user attaches a file, it will show as the last listing.

--- resources/views/livewire/project-edit.blade.php
+++ resources/views/livewire/project-edit.blade.php
@@ -87,6 +87,7 @@
                     </dt>
                     <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
 
+                        
                         <!-- Beginningof Unordered List of Attachments -->
                         <ul role="list" class="border border-gray-200 rounded-md divide-y divide-gray-200">
 
@@ -94,46 +95,32 @@
                                 Attachment 1
                                 The 'For Loop' to display list of attachment should go here.
                              -->
-                            <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                                <div class="w-0 flex-1 flex items-center">
+                             @foreach($attachments as $attachment)
+                                <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                                    <div class="w-0 flex-1 flex items-center">
 
-                                    <!-- Heroicon name: solid/paper-clip -->
-                                    <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                        <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
-                                    </svg>
+                                        <!-- Heroicon name: solid/paper-clip -->
+                                        <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                            <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
+                                        </svg>
 
-                                    <!-- Name of File -->
-                                    <span class="ml-2 flex-1 w-0 truncate">
-                                        Sketcher_Drawings.pdf
-                                    </span>
-                                </div>
-                                <div class="ml-4 flex-shrink-0">
-                                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                                        Download
-                                    </a>
-                                </div>
-                            </li>
+                                        <!-- Name of File -->
+                                        <span class="ml-2 flex-1 w-0 truncate">
+                                            {{ $attachment->file_name }}
+                                        </span>
+                                    </div>
 
-                            <!-- 
-                                Attachment 2
-                                The 'End For Loop' to finish the loop should go here.  We can remove the second list item.
-                             -->
-                            <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                                <div class="w-0 flex-1 flex items-center">
-                                    <!-- Heroicon name: solid/paper-clip -->
-                                    <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                        <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
-                                    </svg>
-                                    <span class="ml-2 flex-1 w-0 truncate">
-                                        submittals.pdf
-                                    </span>
-                                </div>
-                                <div class="ml-4 flex-shrink-0">
-                                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                                        Download
-                                    </a>
-                                </div>
-                            </li>
+                                    <!-- 
+                                        The attachment file path is incorrect.  The temporary file path is being saved. I need to get the correct link from S3 so I can save it on the database and post a link.
+                                     -->
+                                    <div class="ml-4 flex-shrink-0">
+                                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+                                        {{ $attachment->file_path }}
+                                        </a>
+                                    </div>
+                                </li>
+                            @endforeach
+
                         </ul>
                     </dd>
                 </div>
