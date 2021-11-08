e2ab2ee57a839ff7f2183481607a7a3ac4b0704b
David Lopez <dlopez@mattmen.com>
2021-11-08 15:17:34 -0500

    Project Form Attachments. I created a way for users to attach multiple files at once when they open up a new project.  The files are saved directly on Amazon S3 Bucket, and the attachment information is on a MySQL Attachment table.

--- app/Http/Livewire/ProjectForm.php
+++ app/Http/Livewire/ProjectForm.php
@@ -4,16 +4,17 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Project;
+use App\Models\Attachment;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rules\Unique;
 use Cviebrock\EloquentSluggable\Services\SlugService;
-use Livewire\WithFileUploads;
+use Livewire\WithFileUploads;  // Required for attachments
 use Livewire\WithPagination;
 
 class ProjectForm extends Component
 {
 
-    use WithFileUploads;
+    use WithFileUploads;  // Required for attachments
     use WithPagination;
 
     public $project_name;
@@ -26,10 +27,15 @@ class ProjectForm extends Component
     public $description;
     public $status;
     public $user_id;
+    public $project;
     public $showModalForm = false;
     public $slug;
-    public $attachments;
-    // public $slug = SlugService::createSlug(Project::class, 'slug', 'My First Post');
+    public $attachments = [];  // Array Variable to save attachments that are uploaded from form    
+    public $attachment = [];  // Variable to save attachment / Required for attachment
+    public $file_name; // Variable to save attachment / Required for attachment
+    public $file; //Variable to save file / Required for attachment
+    public $current_project_id;
+    
 
     public function showCreateProjectModal() {
         
@@ -51,9 +57,12 @@ class ProjectForm extends Component
             'zip' => 'required',
             'description' => 'required',
             'status' => 'required',
-            // 'attachments' => 'file|mimes:pdf,jpg,png|max:102400 ', // 12MB Max, and must be an image or pdf.
+            'attachments.*' => 'max:102400 ', // Requiired validation for attachments
         ]);
 
+        
+        
+        // Creata a new Project object and save information for MySQl
         $project =new Project();
         $project->user_id = auth()->user()->id;
         $project->project_name = $this->project_name;
@@ -66,9 +75,24 @@ class ProjectForm extends Component
         $project->zip = $this->zip;
         $project->description = $this->description;
         $project->status = $this->status;
-        // $project->attachments = $this->attachments;
+
+        $project->save(); 
         
-        $project->save();
+        // I created a variable to save the current project ID.  I used the current project id and saved it in the attachment table as project_id in the for loop below.
+        $this->current_project_id = $project->id;
+
+        // Store the uploaded attachments in the "attachments" directory of the default filesystem disk and S3
+        foreach ($this->attachments as $this->attachment) {
+            $file = new Attachment(); // Create a new Attachment object that will store Store info for the attachment table in MySQL.
+            $this->attachment->store('attachments', 's3'); // Save attachment on Amazon S3// Store in the "attachment" directory.
+            $file->project_id = $this->current_project_id;  // Fetch the current project id and save it in the project_id column of the files table.
+            $file->user_id = auth()->user()->id; // Fetch the authenticated users's id and save it to the user_id column of the files table.
+            $file->file_name = $this->attachment->getClientOriginalName(); // Save the original name of the file.
+            $file->file_path = $this->attachment; // Save the Amazon S3 address to file_path
+            $file->save(); // Save instance onto the MySQL row.
+        }
+        
+        
         $this->reset();
     }
 
