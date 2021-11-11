08da3f2a6442bcdb6df28a3ba1da87cdd4ea24b0
David Lopez <dlopez@mattmen.com>
2021-11-11 15:53:22 -0500

    Project City Drop Down. I changed the city field into a drop down and provided a few selections since the application is only going to be used in NYC.

--- resources/views/livewire/project-form.blade.php
+++ resources/views/livewire/project-form.blade.php
@@ -98,10 +98,18 @@
 
                     <!-- City Field -->
                     <div class="w-full px-5 mb-6">
-                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-street">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
                             City
                         </label>
-                        <input wire:model.lazy="city" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" name="grid-city" type="text" placeholder="Enter city">
+                        <select wire:model.lazy="city" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" name="grid-city">
+                            <option value=""selected>Select City</option>
+                            <option value="Brooklyn">Brooklyn</option>
+                            <option value="Bronx">Bronx</option>
+                            <option value="Queens">Queens</option>
+                            <option value="Manhattan">Manhattan</option>
+                            <option value="Staten Island">Staten Island</option>
+                            <option value="Long Island">Long Island</option>
+                        </select>
                         @error('city') <span class="error text-red-700 font-bold">{{ $message }}</span> @enderror
                     </div>
 
