35ef188d10ecf5a3ff1930973cddb58f40951e40
David Lopez <dlopez@mattmen.com>
2021-11-17 12:07:37 -0500

    Assign Mechanic Component. I began added the variables needed for the component.  I also added the returned the existing users on the application.

--- app/Http/Livewire/AssignMechanics.php
+++ app/Http/Livewire/AssignMechanics.php
@@ -4,9 +4,16 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\User;
+use App\Models\Project;
 
 class AssignMechanics extends Component
 {
+
+    // Establish Public Variables for component.
+    public $selected_project_id; // This varialble will store the selected project id
+    public $selected_mechanic_id;  // This variable will save the id for the mechanic selected on the components drop down.
+
+
 
     /** 
      * Create a method an call it assignMechanicToThisProject
@@ -14,13 +21,25 @@ class AssignMechanics extends Component
      */ 
     public function assignMechanicToThisProject() 
     {
-        // Crab selected project id.
-        // Grab mechanic id from input selection. 
-        // Store record on project_user table.
+        
+        $this->selected_mechanic_id;
+        $this->selected_project_id;
+
+        // $assignment = new ProjectUser();
+        //     $assignment->project_id = $this->selected_project_id;
+        //     $assignment->user->id = $this->selected_mechanic_id;
+        // $assignment->save();
+
+        $project = Project::find($this->selected_project_id);  // Find the selected project ID.
+        $project->users()->attach($this->selected_mechanic_id);  // 
+
+        
     }
 
     public function render()
     {
-        return view('livewire.assign-mechanics');
+        return view('livewire.assign-mechanics', [
+            'users' => User::all()
+        ]);
     }
 }
