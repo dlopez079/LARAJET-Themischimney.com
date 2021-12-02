3da9c171d79a0f6fca8770f0513a381699b6187e
David Lopez <dlopez@mattmen.com>
2021-12-02 15:30:06 -0500

    Client Details Modification. I removed some of the details from the clients details blade.  Eventually I'm going to change this from clients to contacts.

--- resources/views/livewire/client-details.blade.php
+++ resources/views/livewire/client-details.blade.php
@@ -37,14 +37,14 @@
                 <!-- Client Address -->
                 <div class="py-2 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                     <dt class="text-sm font-medium text-gray-500">
-                        Site / Location
+                        Address
                     </dt>
                     <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                         {{ $selected_street}} {{ $selected_city}} {{ $selected_state }} {{ $selected_zip}}
                     </dd>
                 </div>
 
-                <!-- Client Contact  -->
+                <!-- Client Contact (s)  -->
                 <div class="py-2 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                     <dt class="text-sm font-medium text-gray-500">
                         Contact
@@ -84,7 +84,7 @@
                     </dd>
                 </div>
 
-                <!-- Client Description -->
+                <!-- Client Notes -->
                 <div class="py-2 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                     <dt class="text-sm font-medium text-gray-500">
                         Description
@@ -93,74 +93,6 @@
                         {{ $selected_description }}
                     </dd>
                 </div>
-
-                <!-- Upload Client Attachments. -->
-                <div class="py-2 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <livewire:upload-files></livewire:upload-files>
-                </div>
-
-                <!-- Client Attachements -->
-                <div class="py-2 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <dt class="text-sm font-medium text-gray-500">
-                        Attachments
-                    </dt>
-                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-
-                        <!-- Beginning of Unordered List of Attachments -->
-                        <ul role="list" class="border border-gray-200 rounded-md divide-y divide-gray-200">
-
-                            <!-- 
-                                Attachment 1
-                                The 'For Loop' to display list of attachment should go here 
-                            -->
-                            <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-
-                                <div class="w-0 flex-1 flex items-center">
-
-                                    <!-- Heroicon name: solid/paper-clip -->
-                                    <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                        <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
-                                    </svg>
-
-                                    <!-- Name of File -->
-                                    <span class="ml-2 flex-1 w-0 truncate">
-                                        Sketcher_Drawings.pdf
-                                    </span>
-                                </div>
-
-                                <div class="ml-4 flex-shrink-0">
-                                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                                        Download <!-- Name of the file -->
-                                    </a>
-                                </div>
-                            </li>
-
-                            <!-- 
-                                Attachment 1
-                                The 'End of For Loop' to display list of attachment should go here 
-                                The second list items should be removed after for loop is established.
-                            -->
-                            <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                                <div class="w-0 flex-1 flex items-center">
-
-                                    <!-- Heroicon name: solid/paper-clip -->
-                                    <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                        <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
-                                    </svg>
-                                    <span class="ml-2 flex-1 w-0 truncate">
-                                        submittals.pdf <!-- Name of the file -->
-                                    </span>
-                                </div>
-                                <div class="ml-4 flex-shrink-0">
-                                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                                        Download
-                                    </a>
-                                </div>
-                            </li>
-                        </ul>
-                    </dd>
-                </div>
-
 
             </dl>
         </div>
