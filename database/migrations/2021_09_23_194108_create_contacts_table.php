5218aef62bffdc0ea12f6ea9d7f44feb3e26090d
David Lopez <dlopez@mattmen.com>
2021-12-03 10:35:08 -0500

    Contact migration Modification. This is a column that will hold the company ID the type refers to. This completes the one to many relationship between contacts and company.

--- database/migrations/2021_09_23_194108_create_contacts_table.php
+++ database/migrations/2021_09_23_194108_create_contacts_table.php
@@ -17,6 +17,7 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('company_id'); // This is a column that will hold the company ID the type refers to. 
             $table->string('first_name');
             $table->string('last_name')->index();  // Last name is the column that will be searched the most so I made that the index.
             $table->string('title');
