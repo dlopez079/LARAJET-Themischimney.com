84bdab991fb4ab2573489fdd6819ea56e53399e0
David Lopez <dlopez@mattmen.com>
2021-12-03 11:58:28 -0500

    Companies Table Modification. I changed the contact_type_id to company_type_id. I also changed the status type from string to boolean and set the default to True.  The only time that a company will not be active upon entering in system is when a website visitor enters their information online.  That info will be set to false and displayed on the dashboard for someonne to call back and make active.

--- database/migrations/2021_12_03_010818_create_companies_table.php
+++ database/migrations/2021_12_03_010818_create_companies_table.php
@@ -16,11 +16,11 @@ class CreateCompaniesTable extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name'); // Created to hold the name of the company
-            $table->string('contact_type_id'); // A column to hold the contact type for ths company/client.
+            $table->string('company_type_id'); // A column to hold the contact type for ths company/client.
             $table->string('address'); // Creatd to hold the address ID for the address of the company that will be saved on another table.
             $table->string('main_line'); // Created to hold the main line of the company. 
             $table->string('website'); // Created to hold the website of the company. 
-            $table->string('status');  // Creatd to maintain the status of the company. 
+            $table->boolean('status')->default('true');  // Created to maintain the status of the company. When a record is entered the default will automatically be true.
             $table->timestamps();
         });
     }
