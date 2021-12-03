40ed9cb444013e2b891cd3bf48c644ed34b74684
David Lopez <dlopez@mattmen.com>
2021-12-03 12:39:35 -0500

    AddressCompany Modification. I corrected the references and the id's for the table.  I appled the unsigned attribute to the id's of the table.

--- database/migrations/2021_12_03_151208_create_address_company_table.php
+++ database/migrations/2021_12_03_151208_create_address_company_table.php
@@ -14,13 +14,13 @@ class CreateAddressCompanyTable extends Migration
     public function up()
     {
         Schema::create('address_company', function (Blueprint $table) {
-            $table->bigInteger('address_id'); // This colum will hold the address_id of the address the company will reference.
-            $table->bigInteger('company_id'); // This column will hold the company_id of the company the address will reference.
+            $table->bigInteger('address_id')->unsigned(); // This column will hold the address_id of the address the company will reference.
+            $table->bigInteger('company_id')->unsigned(); // This column will hold the company_id of the company the address will reference.
             $table->timestamps();
             $table->primary(['address_id','company_id']); 
 
-            $table->foreign('address_id')->references('id')->on('addresss')->onDelete('cascade'); // Take your foreign ID and reference it to the id of the address table.
-            $table->foreign('company_id')->references('id')->on('companys')->onDelete('cascade'); // Take your foreign ID and reference it to the id of the companys table.
+            $table->foreign('address_id')->references('id')->on('addresses')->onDelete('cascade'); // Take your foreign ID and reference it to the id of the address table.
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade'); // Take your foreign ID and reference it to the id of the companys table.
 
         });
     }
