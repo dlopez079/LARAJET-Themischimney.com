cf11b8f217e8d5f4702895f14687a1a1b3c2462e
David Lopez <dlopez@mattmen.com>
2021-12-03 10:31:28 -0500

    Companies Migration Modification. A column to hold the contact type for ths company/client.  This will complete the relationshp between company and contact type.

--- database/migrations/2021_12_03_010818_create_companies_table.php
+++ database/migrations/2021_12_03_010818_create_companies_table.php
@@ -16,6 +16,7 @@ class CreateCompaniesTable extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name'); // Created to hold the name of the company
+            $table->string('contactType_id')
             $table->string('address'); // Creatd to hold the address ID for the address of the company that will be saved on another table.
             $table->string('phone'); // Created to hold the main line of the company. 
             $table->string('website'); // Created to hold the website of the company. 
