f9ce32b1a6154aa22f3d2036ab1e0772d5d2039f
David Lopez <dlopez@mattmen.com>
2021-12-13 11:26:36 -0500

    Create Blade Modifications. I reduced the number of fields to just three to simplify things.  I confirmed that the create form works and the show blade is listing the company that is saved on the Mysql database.

--- app/Http/Livewire/Contacts/Create.php
+++ app/Http/Livewire/Contacts/Create.php
@@ -39,7 +39,7 @@ class Create extends Component
     protected function getListeners()
     {
         return [
-            'showCreateContactModal' => 'showCreateContactModal',
+            'showCreateCompanyModal' => 'showCreateCompanyModal',
         ];
     }
     
@@ -49,7 +49,7 @@ class Create extends Component
      * This functionj/method is responsible for displaying the client modal form.
      * The client model form is where you will enter and save client information. 
     */
-    public function showCreateContactModal() {
+    public function showCreateCompanyModal() {
         
         // Change the public variable showModelForm from false to true because you are going from not showing the modal to showing it. 
         $this->showModalForm = true;
@@ -60,29 +60,29 @@ class Create extends Component
      * STORE COMPANY INFO
      * Create a method for storeClient for the Create Client Modal
     */
-    public function storeContact()
+    public function storeCompany()
     {
 
         // Gather information from the Create Client Modal and enter it into MySql.
         $this->validate([
             // Company fieldset
             'name' => 'required',
-            'companyType' => 'require',
+            'companyType' => 'required',
             'mainLine' => 'required | digits:10',
             'website' => 'required | url',
 
-            // Company Address fieldset
-            'street' => 'required',
-            'city' => 'required',
-            'zip' => 'required | max:10',
+            // // Company Address fieldset
+            // 'street' => 'required',
+            // 'city' => 'required',
+            // 'zip' => 'required | max:10',
             
 
-            // Contact Fieldset
-            'firstName' => 'required', 
-            'lastName' => 'required',
-            'title' => 'required', 
-            'email' => 'required | email',
-            'cel' => 'required | digits:10',
+            // // Contact Fieldset
+            // 'firstName' => 'required', 
+            // 'lastName' => 'required',
+            // 'title' => 'required', 
+            // 'email' => 'required | email',
+            // 'cel' => 'required | digits:10',
         ]);
 
         // Create a new Company object/record.
@@ -93,22 +93,22 @@ class Create extends Component
 
             // Company Fieldset
             $company->name = $this->name;
-            $company->type->name = $this->companyType;
-            $company->mainLine = $this->mainLine;
+            $company->type_id = $this->companyType;
+            $company->main_line = $this->mainLine;
             $company->website = $this->website;
 
-            // Address Fieldset
-            $company->addresses->street = $this->street;
-            $company->addresses->street2 = $this->street2;
-            // $company->city = $this->city;  DEFAULT 'NY' ON DATABASE
-            $company->addresses->zip = $this->zip;
+            // // Address Fieldset
+            // $company->addresses->street = $this->street;
+            // $company->addresses->street2 = $this->street2;
+            // // $company->city = $this->city;  DEFAULT 'NY' ON DATABASE
+            // $company->addresses->zip = $this->zip;
 
-            // Contact Fieldset
-            $company->contacts->first_name = $this->firstName;
-            $company->contacts->last_name = $this->lastName;
-            $company->contacts->title = $this->title;
-            $company->contacts->email = $this->email;
-            $company->contacts->cel = $this->cel;
+            // // Contact Fieldset
+            // $company->contacts->first_name = $this->firstName;
+            // $company->contacts->last_name = $this->lastName;
+            // $company->contacts->title = $this->title;
+            // $company->contacts->email = $this->email;
+            // $company->contacts->cel = $this->cel;
         
         $company->save();
         $this->reset();
@@ -118,7 +118,7 @@ class Create extends Component
     {
 
         return view('livewire.contacts.create', [
-            'types' => Type::all()
+            'types' => Type::all(),
        ]);
     }
 }
