962ff2e15f8a4aec578cf1f6920e570ae5b50c74
David Lopez <dlopez@mattmen.com>
2021-12-13 22:43:18 -0500

    Clock in / Clock out. I created the clock out / clock out functionality on the controllers for both create and show.

--- app/Http/Livewire/Timesheet/Create.php
+++ app/Http/Livewire/Timesheet/Create.php
@@ -12,7 +12,6 @@ class Create extends Component
 
     public $showTimesheetModal = false;
     public $activeProjectSelected;
-    public $isWorking;
    
     /**
      * Create listeners for other components.
@@ -35,21 +34,29 @@ class Create extends Component
 
     public function storeTimesheet($id) 
     {  
+        // Gather information from the Create Project Modal and enter it into MySql.
+        // $this->validate([
+        //     'project_name' => 'required',
+        // ]);
+
         $user_id = auth()->user()->id; // Used to track the user that inputs the time.
         $project_id = $id; //Used to track the project that the user is applying time for.
-        $isWorking = true; // Track when the user is working. 
+        $isWorking = 1; // Track when the user is working. 
+        date_default_timezone_set("America/New_York");
 
         $timesheets =new Timesheet();
             $timesheets->user_id = $user_id; // Used to track the user that inputs the time.
             $timesheets->project_id = $project_id;  // Used to track the project that the time is charged to.
             $timesheets->isWorking = $isWorking; // Use to see if user is actively working.
+            $timesheets->date = date('m/j/y'); // Record time stamp.
+            $timesheets->time_from = date("h:i:s A");
         
         $timesheets->save(); 
 
         if($timesheets->save())
         {
             // Emit to project edit to refresh the selected project.  
-            return redirect()->to('/projects');
+            return redirect()->to('/timesheets');
         };
 
         $this->reset();
@@ -65,8 +72,6 @@ class Create extends Component
         // I returned to the view all the project that the user is assigned to. 
         return view('livewire.timesheet.create', [
             'user' => User::find($currentUserId),
-            'projects' => $this->activeProjectSelected,
-            
         ]);
     }
 }
--- app/Http/Livewire/Timesheet/Show/Mechanic.php
+++ app/Http/Livewire/Timesheet/Show/Mechanic.php
@@ -4,9 +4,26 @@ namespace App\Http\Livewire\Timesheet\Show;
 
 use Livewire\Component;
 use App\Models\User;
+use App\Models\Timesheet;
 
 class Mechanic extends Component
 {
+
+    public function timeTo(Timesheet $timesheet) 
+    {
+        //Created a function to clock out.
+        $timesheet = Timesheet::find($timesheet->id);
+        date_default_timezone_set("America/New_York");
+        $timesheet->time_to = date("h:i:s A");
+        $timesheet->save(); 
+
+        if($timesheet->save())
+        {
+            // Emit to project edit to refresh the selected project.  
+            return redirect()->to('/timesheets');
+        };
+    }
+
     public function render()
     {
         $currentUserId = auth()->user()->id; // Used to track the user that inputs the time.
