533d7daca72d29c0bb1fe941b6120711b65748ce
David Lopez <dlopez@mattmen.com>
2021-12-13 22:45:26 -0500

    Removed Slug Entry. I removed the slug entry from the timesheet model.

--- app/Models/Timesheet.php
+++ app/Models/Timesheet.php
@@ -2,25 +2,16 @@
 
 namespace App\Models;
 
-use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Timesheet extends Model
 {
     use HasFactory;
-    use Sluggable;
+
 
     protected $guarded = [];
 
-    public function sluggable(): array
-    {
-        return [
-            'slug' => [
-                'source' => 'project', 'id'
-            ]
-        ];
-    }
 
     /**
      * A timesheet belongs to a project
