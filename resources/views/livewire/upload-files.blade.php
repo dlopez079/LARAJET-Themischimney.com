cd791f71e864a40c5f53797eec460d3b9295705a
David Lopez <dlopez@mattmen.com>
2021-12-14 10:02:26 -0500

    Vapor: Upload Files. Files are not uploading to the Amazing S3 environment.  I added attributes to the input tag within the upload blade in preperation for Vapor to work with S3.

--- resources/views/livewire/upload-files.blade.php
+++ resources/views/livewire/upload-files.blade.php
@@ -10,7 +10,7 @@
             <form class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 @csrf
                 <div class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    <input type="file" wire:model="files" multiple>
+                    <input type="file" id="file" ref="file" wire:model="files" multiple>
                     @error('files.*') <span class="error">{{ $message }}</span> @enderror
                 </div>
             </form>
